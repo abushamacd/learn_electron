2c705aaf1ab3a3079e59ef4cf2a631e13e93e193
abushamacd <assiddik001@gmail.com>
2025-04-27 10:11:22 +0600

    zero to context isolation

---
+++ main.js
@@ -0,0 +1,48 @@
+const { app, BrowserWindow } = require("electron/main");
+const windowStateKeeper = require("electron-window-state");
+const path = require("path");
+
+const createWindow = () => {
+  let mainWindowState = windowStateKeeper({
+    defaultWidth: 800,
+    defaultHeight: 1000,
+  });
+
+  const win = new BrowserWindow({
+    x: mainWindowState.x,
+    y: mainWindowState.y,
+    width: mainWindowState.width,
+    height: mainWindowState.height,
+    title: "Web Title",
+    webPreferences: {
+      // nodeIntegration: true,
+      contextIsolation: false,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  win.loadFile("index.html");
+  // win.loadFile("index.html");
+
+  if (process.env.mode !== "production") {
+    win.webContents.openDevTools();
+  }
+
+  mainWindowState.manage(win);
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
---
+++ preload.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("button").addEventListener("click", () => {
+    console.log("Test");
+  });
+});
+
+window.test = "testing";
